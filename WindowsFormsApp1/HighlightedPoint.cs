d8248adbd55370ecfcc8184d6971bff56b048906
aisen <xxxAuronblade277xxx@gmail.com>
2021-10-25 17:19:08 +0300

    Final Fix

---
+++ WindowsFormsApp1/HighlightedPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Dynamic;
+using ScottPlot;
+using ScottPlot.Plottable;
+
+namespace WindowsFormsApp1
+{
+    public class HighlightedPoint
+    {
+        private ScatterPlot point;
+        private int index = -1;
+
+        public HighlightedPoint(FormsPlot plot)
+        {
+            point = plot.Plot.AddPoint(0, 0);
+            point.Color = Color.Red;
+            point.MarkerSize = 10;
+            point.MarkerShape = ScottPlot.MarkerShape.openCircle;
+            point.IsVisible = false;
+        }
+
+        public void isVisible(bool visibility)
+        {
+            point.IsVisible = visibility;
+        }
+
+        public void changePoint(double x, double y)
+        {
+            point.Xs[0] = x;
+            point.Ys[0] = y;
+        }
+
+        public int GetIndex()
+        {
+            return index;
+        }
+
+        public void setIndex(int newIndex)
+        {
+            index = newIndex;
+        }
+        
+    }
+}
--- WindowsFormsApp1/Plot.cs
+++ WindowsFormsApp1/Plot.cs
@@ -3,93 +3,54 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using Microsoft.FSharp.Data.UnitSystems.SI.UnitNames;
 using ScottPlot.Plottable;
 
 namespace WindowsFormsApp1
 {
     public partial class Plot : Form
     {
-        private double x0;
-        private double y0;
-        private double X;
-        private int N;
-        private int Nmin;
-        private int Nmax;
-
-        private Exact exact;
-        private Euler euler;
-        private ImprovedEuler improvedEuler;
-        private RungeKutta rungeKutta;
-
-        private ScottPlot.Plottable.ScatterPlot exactGraph;
-        private ScottPlot.Plottable.ScatterPlot eulerGraph;
-        private ScottPlot.Plottable.ScatterPlot improvedEulerGraph;
-        private ScottPlot.Plottable.ScatterPlot rungeGraph;
-        private ScottPlot.Plottable.ScatterPlot eulerErrorGraph;
-        private ScottPlot.Plottable.ScatterPlot improvedEulerErrorGraph;
-        private ScottPlot.Plottable.ScatterPlot rungeErrorGraph;
-        private ScottPlot.Plottable.ScatterPlot eulerMaxErrorGraph;
-        private ScottPlot.Plottable.ScatterPlot improvedEulerMaxErrorGraph;
-        private ScottPlot.Plottable.ScatterPlot rungeMaxErrorGraph;
-        
         private List<ScatterPlot> secondform_graphs = new List<ScatterPlot>();
         private List<ScatterPlot> firstform_graphs = new List<ScatterPlot>();
         private List<ScatterPlot> thirdform_graphs = new List<ScatterPlot>();
 
-        private ScottPlot.Plottable.ScatterPlot HighlightedPointFirstGraph;
-        private int LastHighlightedIndexFirstGraph = -1;
+        private HighlightedPoint firstHP;
+        private HighlightedPoint secondHP;
+        private HighlightedPoint thirdHP;
 
-        private ScottPlot.Plottable.ScatterPlot HighlightedPointSecondGraph;
-        private int LastHighlightedIndexSecondGraph = -1;
-
-        private ScottPlot.Plottable.ScatterPlot HighlightedPointThirdGraph;
-        private int LastHighlightedIndexThirdGraph = -1;
-
-        void ThirdGraphDraw()
+        void ThirdGraphDraw(int Nmin, int Nmax, double x0, double y0, double X)
         {
             formsPlot3.Plot.Clear();
             formsPlot3.Plot.Title("Max Error");
-            
-            HighlightedPointThirdGraph = formsPlot3.Plot.AddPoint(0, 0);
-            HighlightedPointThirdGraph.Color = Color.Red;
-            HighlightedPointThirdGraph.MarkerSize = 10;
-            HighlightedPointThirdGraph.MarkerShape = ScottPlot.MarkerShape.openCircle;
-            HighlightedPointThirdGraph.IsVisible = false;
+
+            thirdHP = new HighlightedPoint(formsPlot3);
 
             MaxError maxError = new MaxError(Nmin, Nmax, x0, y0, X);
             List<Grid> grids = new List<Grid>();
-            grids.AddRange(new List<Grid>{
+            grids.AddRange(new List<Grid>
+            {
                 maxError.eulerMaxError(),
                 maxError.eulerImproveMaxError(),
                 maxError.rungeKuttaMaxError(),
             });
             GraphDraw graphDraw = new GraphDraw(formsPlot3, grids);
             thirdform_graphs = graphDraw.returnPlots();
-            
-            eulerMaxErrorGraph = thirdform_graphs.First(g => g.Label == "euler");
-            improvedEulerMaxErrorGraph = thirdform_graphs.First(g => g.Label == "improved euler");
-            rungeMaxErrorGraph = thirdform_graphs.First(g => g.Label == "runge kutta");
-            
-            eulerMaxErrorGraph.IsVisible = checkBox2.Checked;
-            improvedEulerMaxErrorGraph.IsVisible = checkBox3.Checked;
-            rungeMaxErrorGraph.IsVisible = checkBox4.Checked;
-            
-            
+
+            thirdform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
+            thirdform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
+            thirdform_graphs.First(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
+
             formsPlot3.Refresh();
             formsPlot3.Plot.Legend();
         }
-        
-        
-        void SecondGraphDraw()
+
+
+        void SecondGraphDraw(Exact exact, Euler euler, ImprovedEuler improvedEuler, RungeKutta rungeKutta)
         {
             formsPlot2.Plot.Clear();
             formsPlot2.Plot.Title("Local Errors");
 
-            HighlightedPointSecondGraph = formsPlot2.Plot.AddPoint(0, 0);
-            HighlightedPointSecondGraph.Color = Color.Red;
-            HighlightedPointSecondGraph.MarkerSize = 10;
-            HighlightedPointSecondGraph.MarkerShape = ScottPlot.MarkerShape.openCircle;
-            HighlightedPointSecondGraph.IsVisible = false;
+            secondHP = new HighlightedPoint(formsPlot2);
 
             LTEerror eulerError = new LTEerror(exact, euler);
             LTEerror eulerimproveError = new LTEerror(exact, improvedEuler);
@@ -104,29 +65,21 @@ namespace WindowsFormsApp1
             GraphDraw graphDraw = new GraphDraw(formsPlot2, grids);
             secondform_graphs = graphDraw.returnPlots();
 
-            eulerErrorGraph = secondform_graphs.First(g => g.Label == "euler");
-            improvedEulerErrorGraph = secondform_graphs.First(g => g.Label == "improved euler");
-            rungeErrorGraph = secondform_graphs.First(g => g.Label == "runge kutta");
-            
-            eulerErrorGraph.IsVisible = checkBox2.Checked;
-            improvedEulerErrorGraph.IsVisible = checkBox3.Checked;
-            rungeErrorGraph.IsVisible = checkBox4.Checked;
-            
-            
+            secondform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
+            secondform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
+            secondform_graphs.First(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
+
             formsPlot2.Refresh();
             formsPlot2.Plot.Legend();
         }
 
 
-        void FirstGraphDraw()
+        void FirstGraphDraw(Exact exact, Euler euler, ImprovedEuler improvedEuler, RungeKutta rungeKutta)
         {
             formsPlot1.Plot.Clear();
             formsPlot1.Plot.Title("Values");
-            HighlightedPointFirstGraph = formsPlot1.Plot.AddPoint(0, 0);
-            HighlightedPointFirstGraph.Color = Color.Red;
-            HighlightedPointFirstGraph.MarkerSize = 10;
-            HighlightedPointFirstGraph.MarkerShape = ScottPlot.MarkerShape.openCircle;
-            HighlightedPointFirstGraph.IsVisible = false;
+
+            firstHP = new HighlightedPoint(formsPlot1);
 
             List<Grid> listofGrids = new List<Grid>();
             listofGrids.AddRange(new List<Grid>
@@ -141,17 +94,11 @@ namespace WindowsFormsApp1
 
             firstform_graphs = graphDraw.returnPlots();
 
-            exactGraph = firstform_graphs.First(g => g.Label == "exact");
-            eulerGraph = firstform_graphs.First(g => g.Label == "euler");
-            improvedEulerGraph = firstform_graphs.First(g => g.Label == "improved euler");
-            rungeGraph = firstform_graphs.Find(g => g.Label == "runge kutta");
-
-            exactGraph.IsVisible = checkBox1.Checked;
-            eulerGraph.IsVisible = checkBox2.Checked;
-            improvedEulerGraph.IsVisible = checkBox3.Checked;
-            rungeGraph.IsVisible = checkBox4.Checked;
-
-            formsPlot1.Plot.SetAxisLimits(x0, X, exactGraph.Ys[N - 1], y0);
+            firstform_graphs.First(g => g.Label == "exact").IsVisible = checkBox1.Checked;
+            firstform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
+            firstform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
+            firstform_graphs.Find(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
+            
             formsPlot1.Refresh();
             formsPlot1.Plot.Legend();
         }
@@ -159,28 +106,29 @@ namespace WindowsFormsApp1
         public Plot()
         {
             InitializeComponent();
-            
+            this.Text = "Computational Practicum Zhylkybay Aisen";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                x0 = Double.Parse(textBox2.Text);
-                y0 = Double.Parse(textBox3.Text);
-                X = Double.Parse(textBox1.Text);
-                N = Int32.Parse(textN.Text);
-                Nmin = Int32.Parse(textBox5.Text);
-                Nmax = Int32.Parse(textBox4.Text);
+                double x0 = x0 = Double.Parse(textBox2.Text);
+                double y0 = Double.Parse(textBox3.Text);
+                double X = Double.Parse(textBox1.Text);
+                int N = Int32.Parse(textN.Text);
+                int Nmin = Int32.Parse(textBox5.Text);
+                int Nmax = Int32.Parse(textBox4.Text);
 
-                exact = new Exact(N, x0, y0, X);
-                euler = new Euler(N, x0, y0, X);
-                improvedEuler = new ImprovedEuler(N, x0, y0, X);
-                rungeKutta = new RungeKutta(N, x0, y0, X);
+                Exact exact = new Exact(N, x0, y0, X);
+                Euler euler = new Euler(N, x0, y0, X);
+                ImprovedEuler improvedEuler = new ImprovedEuler(N, x0, y0, X);
+                RungeKutta rungeKutta = new RungeKutta(N, x0, y0, X);
 
-                ThirdGraphDraw();
-                SecondGraphDraw();
-                FirstGraphDraw();
+
+                ThirdGraphDraw(Nmin,Nmax,x0,y0,X);
+                SecondGraphDraw(exact,euler,improvedEuler,rungeKutta);
+                FirstGraphDraw(exact,euler,improvedEuler,rungeKutta);
             }
             catch (Exception exception)
             {
@@ -191,15 +139,15 @@ namespace WindowsFormsApp1
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            exactGraph.IsVisible = checkBox1.Checked;
+            firstform_graphs.First(g => g.Label == "exact").IsVisible = checkBox1.Checked;
             formsPlot1.Refresh();
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-            eulerGraph.IsVisible = checkBox2.Checked;
-            eulerErrorGraph.IsVisible = checkBox2.Checked;
-            eulerMaxErrorGraph.IsVisible = checkBox2.Checked;
+            firstform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
+            secondform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
+            thirdform_graphs.First(g => g.Label == "euler").IsVisible = checkBox2.Checked;
             formsPlot1.Refresh();
             formsPlot2.Refresh();
             formsPlot3.Refresh();
@@ -207,9 +155,9 @@ namespace WindowsFormsApp1
 
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
-            improvedEulerGraph.IsVisible = checkBox3.Checked;
-            improvedEulerErrorGraph.IsVisible = checkBox3.Checked;
-            improvedEulerMaxErrorGraph.IsVisible = checkBox3.Checked;
+            firstform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
+            secondform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
+            thirdform_graphs.First(g => g.Label == "improved euler").IsVisible = checkBox3.Checked;
             formsPlot1.Refresh();
             formsPlot2.Refresh();
             formsPlot3.Refresh();
@@ -217,9 +165,9 @@ namespace WindowsFormsApp1
 
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
-            rungeGraph.IsVisible = checkBox4.Checked;
-            rungeErrorGraph.IsVisible = checkBox4.Checked;
-            rungeMaxErrorGraph.IsVisible = checkBox4.Checked;
+            firstform_graphs.Find(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
+            secondform_graphs.First(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
+            thirdform_graphs.First(g => g.Label == "runge kutta").IsVisible = checkBox4.Checked;
             formsPlot1.Refresh();
             formsPlot2.Refresh();
             formsPlot3.Refresh();
@@ -233,14 +181,12 @@ namespace WindowsFormsApp1
             double xyRatio = formsPlot1.Plot.XAxis.Dims.PxPerUnit / formsPlot1.Plot.YAxis.Dims.PxPerUnit;
             (double pointX, double pointY, int pointIndex) = minPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
 
-            HighlightedPointFirstGraph.Xs[0] = pointX;
-            HighlightedPointFirstGraph.Ys[0] = pointY;
+            firstHP.changePoint(pointX,pointY);
+            firstHP.isVisible(true);
 
-            HighlightedPointFirstGraph.IsVisible = true;
-
-            if (LastHighlightedIndexFirstGraph != pointIndex)
+            if (firstHP.GetIndex() != pointIndex)
             {
-                LastHighlightedIndexFirstGraph = pointIndex;
+                firstHP.setIndex(pointIndex);
                 formsPlot1.Render();
             }
 
@@ -259,14 +205,12 @@ namespace WindowsFormsApp1
             double xyRatio = formsPlot2.Plot.XAxis.Dims.PxPerUnit / formsPlot2.Plot.YAxis.Dims.PxPerUnit;
             (double pointX, double pointY, int pointIndex) = minPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
 
-            HighlightedPointSecondGraph.Xs[0] = pointX;
-            HighlightedPointSecondGraph.Ys[0] = pointY;
+            secondHP.changePoint(pointX,pointY);
+            secondHP.isVisible(true);
 
-            HighlightedPointSecondGraph.IsVisible = true;
-
-            if (LastHighlightedIndexSecondGraph != pointIndex)
+            if (secondHP.GetIndex() != pointIndex)
             {
-                LastHighlightedIndexSecondGraph = pointIndex;
+                secondHP.setIndex(pointIndex);
                 formsPlot2.Render();
             }
 
@@ -291,14 +235,12 @@ namespace WindowsFormsApp1
             double xyRatio = formsPlot3.Plot.XAxis.Dims.PxPerUnit / formsPlot3.Plot.YAxis.Dims.PxPerUnit;
             (double pointX, double pointY, int pointIndex) = minPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
 
-            HighlightedPointThirdGraph.Xs[0] = pointX;
-            HighlightedPointThirdGraph.Ys[0] = pointY;
+            thirdHP.changePoint(pointX,pointY);
+            thirdHP.isVisible(true);
 
-            HighlightedPointThirdGraph.IsVisible = true;
-
-            if (LastHighlightedIndexThirdGraph != pointIndex)
+            if (thirdHP.GetIndex() != pointIndex)
             {
-                LastHighlightedIndexThirdGraph = pointIndex;
+                thirdHP.setIndex(pointIndex);
                 formsPlot3.Render();
             }
 
